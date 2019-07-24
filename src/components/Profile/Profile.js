06ccab0ba9fe6f9198bd2142ab0ecbe10dd3c3dd
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-07-23 19:41:41 -0500

    create is a go

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -2,13 +2,54 @@ import React from 'react';
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import { Link } from 'react-router-dom';
+import {
+  Button, Modal, ModalHeader, ModalBody, ModalFooter,
+} from 'reactstrap';
 import RoutineCard from '../RoutineCard/RoutineCard';
 import routineData from '../../helpers/data/routineData';
 import './Profile.scss';
 
+const defaultRoutine = {
+  name: '',
+  uid: '',
+};
+
 class Profile extends React.Component {
-  state = {
-    routines: [],
+  constructor(props) {
+    super(props);
+    this.state = {
+      modal: false,
+      routines: [],
+      newRoutine: defaultRoutine,
+    };
+
+    this.toggle = this.toggle.bind(this);
+  }
+
+  toggle() {
+    this.setState(prevState => ({
+      modal: !prevState.modal,
+    }));
+  }
+
+  formFieldStringState = (name, e) => {
+    const tempRoutine = { ...this.state.newRoutine };
+    tempRoutine[name] = e.target.value;
+    this.setState({ newRoutine: tempRoutine });
+  }
+
+  nameChange = e => this.formFieldStringState('name', e);
+
+  formSubmit = (e) => {
+    e.preventDefault();
+    const saveMe = { ...this.state.newRoutine };
+    saveMe.uid = firebase.auth().currentUser.uid;
+    routineData.postRoutine(saveMe)
+      .then(() => {
+        this.toggle();
+        this.getRoutines();
+      })
+      .catch(err => console.error('unable to save', err));
   }
 
   getRoutines = () => {
@@ -28,10 +69,30 @@ class Profile extends React.Component {
       routine={routine}
       />
     ));
+    const { newRoutine } = this.state;
     return (
       <div className="profile col">
       <h1>My Profile</h1>
       <Link className="btn btn-primary" to='/stretches'>View Stretches</Link>
+      <Button color="danger" onClick={this.toggle}>New Routine</Button>
+        <Modal isOpen={this.state.modal} toggle={this.toggle} className="routineModal">
+          <ModalHeader toggle={this.toggle}>New Routine</ModalHeader>
+          <ModalBody>
+          <label htmlFor="name">Routine Name?</label>
+            <input
+              type="text"
+              className="form-control"
+              id="name"
+              placeholder="Stretch it out"
+              value={newRoutine.name}
+              onChange={this.nameChange}
+            />
+          </ModalBody>
+          <ModalFooter>
+            <Button color="primary" type="submit" onClick={this.formSubmit}>Save Routine</Button>{' '}
+            <Button color="secondary" onClick={this.toggle}>Cancel</Button>
+          </ModalFooter>
+        </Modal>
       <div className="routCards">
       {makeRoutineCards}
       </div>
