0e9fc0ab300d9059e5bb753aed2f8e3ad723e6ae
Matthew Caldwell <MCaldwell4242@gmail.com>
2019-07-24 18:41:53 -0500

    reading routineStretches

---
+++ src/helpers/data/routineStretchData.js
@@ -0,0 +1,22 @@
+import axios from 'axios';
+import apiKeys from '../apiKeys.json';
+
+const firebaseUrl = apiKeys.firebaseKeys.databaseURL;
+
+const getRoutineStretches = routineId => new Promise((resolve, reject) => {
+  axios.get(`${firebaseUrl}/routineStretches.json?orderBy="routineId"&equalTo="${routineId}"`)
+    .then((results) => {
+      const routineStretchResults = results.data;
+      const routineStretches = [];
+      if (routineStretchResults !== null) {
+        Object.keys(routineStretchResults).forEach((routineStretchId) => {
+          routineStretches.push(routineStretchResults[routineStretchId].stretchId);
+        });
+      }
+      console.error(routineStretches);
+      resolve(routineStretches);
+    })
+    .catch(err => reject(err));
+});
+
+export default { getRoutineStretches };
